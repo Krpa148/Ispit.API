508dd7dfbc3a5681443bdf579a98f7e4d48f142a
Alain Kapel <alain.kapel5@gmail.com>
2023-07-27 18:42:53 +0200

    Add project files.

---
+++ Ispit.API/Models/IspitApiDbContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Ispit.API.Models
+{
+    public partial class IspitApiDbContext : DbContext
+    {
+        public IspitApiDbContext()
+        {
+        }
+
+        public IspitApiDbContext(DbContextOptions<IspitApiDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<ToDoList> ToDoLists { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-2BHHQH4\\SQLEXPRESS;Database=IspitApiDb;Trusted_Connection=True;TrustServerCertificate=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ToDoList>(entity =>
+            {
+                entity.ToTable("ToDoList");
+
+                entity.Property(e => e.Description).HasMaxLength(150);
+
+                entity.Property(e => e.Title).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Ispit.API/Program.cs
@@ -0,0 +1,39 @@
+using Ispit.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace Ispit.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<IspitApiDbContext>(options => options.UseSqlServer(connString));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
